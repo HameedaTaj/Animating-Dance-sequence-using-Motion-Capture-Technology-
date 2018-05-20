2f170caa22740dc9ac8ca1f761b53b9b60df863c
HameedaTaj <hameeda.dildar@gmail.com>
2018-05-19 22:58:43 -0400

    Add files via upload

---
+++ Mocap/Assets/Scripts/Animat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animat : MonoBehaviour {
+
+	public Animator ani1;
+	public Animator ani2;
+	public Animator ani3;
+
+	public void start()
+	{
+		ani1.speed=1;
+		ani2.speed=1;
+		ani3.speed=1;
+	}
+
+	public void stop()
+	{
+		ani1.speed=0;
+		ani2.speed=0;
+		ani3.speed=0;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Mocap/Assets/Scripts/init.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour {
+
+	public Animator ani1;
+	public Animator ani2;
+	public Animator ani3;
+
+	// Use this for initialization
+	void Start () {
+		ani1.speed=0;
+		ani2.speed=0;
+		ani3.speed=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
